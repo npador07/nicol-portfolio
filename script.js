184dfa911f9d266e77c828d2156c18b9b2afe3fe
npador07 <npador07@gmail.com>
2025-08-28 09:26:08 +0800

    add buttons to boxes in project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const projects = document.getElementById("grid-container");
 const gridClick = document.querySelectorAll(".grid-item");
 
-gridClick.addEventListener("click", function() {
-    console.log("clicked")
-})
+gridClick.forEach(gridClick => {
+    gridClick.addEventListener("click", ()=> {
+        console.log("clicked")
+    });
+}  );
